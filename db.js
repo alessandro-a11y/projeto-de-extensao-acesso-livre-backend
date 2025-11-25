704a07ae07bfb33767c9fb7db2d449b4d1e75c75
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2025-11-25 00:51:03 -0300

    Use environment variables for MySQL config
    Replaces hardcoded MySQL connection parameters with environment variables for improved security and flexibility. This allows configuration via a .env file and better supports different deployment environments.

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); // Adicione esta linha se estiver usando um arquivo .env
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: process.env.senha_do_mysql,
-    database: 'acessolivre_db',
+    host: process.env.MYSQLHOST ,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
