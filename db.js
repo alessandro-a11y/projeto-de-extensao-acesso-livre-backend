fedff6d21e9b565fba2478fd114cdf8d948f2adf
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2025-11-25 01:54:29 -0300

    Remove extra spaces and add newline in db.js
    Cleaned up spacing in the MySQL host configuration and ensured the file ends with a newline for consistency.

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config(); // Adicione esta linha se estiver usando um arquivo .env
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST ,
+    host: process.env.MYSQLHOST,
     port: process.env.MYSQLPORT,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
